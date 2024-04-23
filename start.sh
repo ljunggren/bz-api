515a778aefa9e049d95a15dd4d1ae71d728346aa
ljunggren <mljunggren@gmail.com>
2024-04-23 17:42:39 -0400

    Added files

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 PORT=7777
 
 # Run npm run dev in the background
-json-server --watch db.json --port $PORT &> /dev/null &
+nohup json-server --watch db.json --port $PORT &> nohup.out &
 
 # Save the PID of the background process to a file
 echo $! > "json-${PORT}.pid"
